6d0efbcf9f46940b8fcd87f7e821ff6ad40acb59
Your Name <you@example.com>
2019-03-19 17:02:48 +0700

    Ultimate-Dork library

---
+++ lib/lib.py
@@ -0,0 +1,115 @@
+#-*- coding: utf-8 -*-
+from mechanicalsoup import StatefulBrowser
+from re import findall
+from .useragent import useragent
+from .tmp import load
+
+class scrape(StatefulBrowser):
+
+      def __repr__(
+        fitur = {
+            'features':'html.parser',
+        },
+        uag = useragent()
+      ):
+          return StatefulBrowser(
+            soup_config = fitur,
+            user_agent = useragent
+          )
+
+class Parser(object):
+
+      __list = []
+
+      def __init__(
+        self,
+        dork,
+        URL,
+        pattern,
+        class_tag,
+        proxy = None
+      ):
+          self.dork = dork
+          self.URL = URL
+          self.__pattern = pattern
+          self.class_tag = class_tag
+          self.proxy = {
+            'https':proxy
+          }
+          
+          
+      def __dir__(self):
+          return list(set(self.__list))
+
+          
+      def get_page(self):
+          self.__req = scrape()
+          self.__req.open(
+            self.URL,
+            proxies = self.proxy,
+            timeout = 10
+          )
+          self.__req.select_form(
+            'form[action="/search"]'
+          )
+          self.__req['q'] = self.dork
+          self.__req.submit_selected()
+          _content = str(self.__req.get_current_page())
+          for urls in findall(
+            self.__pattern,
+            _content                        
+          ):  
+              if 'www.google.com' in self.URL: self.__list.append(urls)
+              else: self.__list.append(urls[:-1])
+          return self.__req.get_current_page().find_all(
+            'a',
+            class_=self.class_tag
+          )
+          
+      def request(self):
+          self.__req = scrape()
+          for page in self.get_page():
+              load()
+              try:
+                   self.__req.open(
+                   f'{self.URL}{page.get("href")}',
+                   proxies = self.proxy,
+                   timeout = 7
+                 )
+                   content = str(self.__req.get_current_page())
+                   for urls in findall(
+                     self.__pattern,
+                     content
+                   ):
+                       if 'www.google.com' in self.URL: self.__list.append(urls)
+                       else: self.__list.append(urls[:-1])
+              except Exception as e:
+                 print(e)
+
+Google = Parser(
+    'filetype:action site:au',
+    'https://www.google.com',
+    'class="r"><a href="/url\?q=(.*?)&amp',
+    'fl'
+)
+Bing = Parser(
+    'index.action',
+    'https://www.bing.com',
+    'h=".*?" href="(h.*?")',
+    "b_widePag sb_bp"
+)       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
---
+++ ultimate-dork.py
@@ -0,0 +1,151 @@
+#-*- coding: utf-8 -*-
+import sys,argparse
+if sys.version[0] in '2':
+   print('\n[x] Not Supported For python 2.x Please Use Python 3.x \n')
+   exit()
+from lib.tmp import color as _
+try:
+    import mechanicalsoup
+    import requests
+except Exception as e:
+    print('\n{}[-]{} mechanicalsoup package Not Installed\n'.format(_.R,_.W))
+    print('type pip3 install mechanicalsoup')
+    exit()
+
+from lib.lib import Parser
+from lib.sqlscan import sqli_scan
+from lib.tmp import logo
+
+urls = []
+
+class crawl(object):
+
+      auth = {
+        1:[
+            'https://www.google.com',
+            'class="r"><a href="/url\?q=(.*?)&amp',
+            'fl'
+        ],
+        2:[
+            'https://www.bing.com',
+            'h=".*?" href="(h.*?")',
+            "b_widePag sb_bp"
+        ]
+      }
+      
+      def __init__(
+        self,
+        dork,
+        proxy = None                
+      ):  
+          self.dork = dork
+          self.proxy = proxy
+          
+      def Bing(self):
+          bing = Parser(
+            self.dork,
+            crawl.auth[1][0],
+            crawl.auth[1][1],
+            crawl.auth[1][2],
+            proxy = self.proxy
+          )
+          bing.request()
+          for url in dir(bing):
+              if 'blogspot.com' in url or 'wordpress.com' in url or 'exploitdb' in url or 'facebook.com' in url or 'pastebin.com' in url:
+                 pass
+              else:
+                 urls.append(url)
+              
+      def Google(self):
+          google = Parser(
+            self.dork,
+            crawl.auth[2][0],
+            crawl.auth[2][1],
+            crawl.auth[2][2],
+            proxy = self.proxy
+          )
+          google.request()
+          for url in dir(google):
+              if 'blogspot.com' in url or 'wordpress.com' in url or 'exploitdb' in url or 'facebook.com' in url or 'bing.com' in url or 'pastebin.com' in url:
+                 pass
+              else:
+                 urls.append(url)
+
+class SQLi_Scanner(sqli_scan):
+      pass
+
+if sys.version[0] in '2':
+   print('\n[x] Not Supported For python 2.x Please Use Python 3.x \n')
+   exit()
+fel = sys.argv[0]
+par = argparse.ArgumentParser(
+prog=fel,
+usage="%(prog)s --dork [keyword]  --scan",
+formatter_class=argparse.RawTextHelpFormatter,
+description="""
+Descriptions:
+ * By : 407 Authentic Exploit
+-----------------------------:
+- Codename : JaxBCD
++ Crawl Web Use Google Dork & Bing Dork
++ with Features SQLi Scanner Vulnerability
+
+""")
+
+par.add_argument(
+'--dork',
+help="""
+Your Dork e.g inurl:.php?id=
+
+""",
+metavar='[keywords]',
+type=str)
+par.add_argument(
+'--proxy',
+help='''
+if using proxy e.g 127.0.0.1:1337
+with auth e.g user@pass:127.0.0.1:1337
+
+''',
+metavar='[proxy:port]',
+type=str,
+action='store',
+default=None)
+par.add_argument(
+'--scan',help="if with Scan SQL injection Vulnerability Use This argument",action="store_true")
+arg = par.parse_args()
+try:
+    if arg.scan:
+       if arg.dork != None:
+           print(logo())
+           _ = crawl(arg.dork,proxy=arg.proxy)
+           _.Bing()
+           _.Google()
+           for url in list(set(urls)):
+               print('- {}'.format(url))
+           for scan in list(set(urls)):
+               SQLi_Scanner().scan(scan)
+       else:
+           par.print_help()        
+    elif not arg.scan:
+       if arg.dork != None:
+          print(logo())  
+          _ = crawl(arg.dork,proxy=arg.proxy)
+          _.Bing()
+          _.Google()
+          for url in list(set(urls)):
+              print('- {}'.format(url))
+       else:
+          par.print_help()              
+    else:           
+       par.print_help()
+except Exception as e:
+    print(e)
+except KeyboardInterrupt:
+    exit()
+        
+
+
+
+
+      
